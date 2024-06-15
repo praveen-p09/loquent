e40dae72a08cc702ca33af54b27b16aa3760f2c4
Praveen Patro <praveen.patro911@gmail.com>
2024-06-15 10:42:59 +0530

    update uploadMiddleware

--- api/middleware/uploadMiddleware.js
+++ api/middleware/uploadMiddleware.js
@@ -1,4 +1,24 @@
 const multer = require("multer");
-const uploadMiddleware = multer({ dest: "uploads/" });
+const os = require("os");
+const path = require("path");
+
+const uploadDir = path.join(os.tmpdir(), "uploads");
+
+// Ensure the temporary directory exists
+const fs = require("fs");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+const uploadMiddleware = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
 
 module.exports = uploadMiddleware;
