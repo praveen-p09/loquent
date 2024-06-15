cf4150f2a7d44401bfc07e19c35ce20f8ddb6f5a
Praveen Patro <praveen.patro911@gmail.com>
2024-06-15 13:19:24 +0530

    update api

--- api/middleware/uploadMiddleware.js
+++ api/middleware/uploadMiddleware.js
@@ -1,24 +1,4 @@
 const multer = require("multer");
-const os = require("os");
-const path = require("path");
-
-const uploadDir = path.join(os.tmpdir(), "uploads");
-
-// Ensure the temporary directory exists
-const fs = require("fs");
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
-
-const uploadMiddleware = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, uploadDir);
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  }),
-});
+const uploadMiddleware = multer({ dest: "uploads/" });
 
 module.exports = uploadMiddleware;
--- api/src/app.js
+++ api/src/app.js
@@ -4,58 +4,42 @@ const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const path = require("path");
-const os = require("os");
 dotenv.config();
 
+const authRoutes = require("../routes/authRoutes.js");
+const postRoutes = require("../routes/postRoutes.js");
+
 const app = express();
-const corsOptions = {
-  origin: "https://loquent.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
 
 // Middleware
+const corsOptions = {
+  origin: "http://localhost:3000", // your frontend domain
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 app.use(cors(corsOptions));
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use("/uploads", express.static(path.join(os.tmpdir(), "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.options("*", cors(corsOptions));
-
-// Debugging endpoint for environment variables
-app.get("/debug/env", (req, res) => {
-  res.json({
-    MONGO_URI: process.env.MONGO_URI || "MONGO_URI is not defined",
-  });
-});
-
 // Database connection
-const uri = process.env.MONGO_URI; // Ensure this environment variable is correctly set
-if (!uri) {
-  console.error(
-    "MONGO_URI is not defined. Please set it in the environment variables."
-  );
-  process.exit(1);
-}
-
-mongoose.connect(uri, {});
-
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 mongoose.connection.on("error", (err) => {
   console.error("MongoDB connection error:", err);
 });
-
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
 });
 
 // Routes
-app.use("/api/auth", require("../routes/authRoutes.js"));
-app.use("/api/posts", require("../routes/postRoutes.js"));
-
-app.get("/", (req, res) => {
-  res.json("Welcome to Loquent API");
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
